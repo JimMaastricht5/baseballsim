2bbba130174f25d4c195f9c2a4f88c949e025ab1
jimmaastricht <jimmaastricht5@gmail.com>
2023-12-31 14:27:36 -0600

    working on interactive lineup changes

--- bbmanager.py
+++ bbmanager.py
@@ -1,3 +1,4 @@
+import json
 
 
 class Manager:
@@ -13,9 +14,9 @@ class Manager:
         while True:
             print("\nOptions:")
             print("0. Accept the Lineup and Start the Game")
-            print("1. Change One Player in the Lineup")
+            print("1. Change a Player in the Lineup")
             print("2. Change Starting Pitcher")
-            # print("3. Enter an entirely new Lineup")
+            print("3. Load Lineup Previous Lineup")
             choice = input("\nEnter your choice: ")
             if choice == "1":
                 # Perform actions for option 1
@@ -24,9 +25,9 @@ class Manager:
             elif choice == "2":
                 # Perform actions for option 2
                 self.interactive_pitching_changes()
-            # elif choice == "3":
-            #     # Perform actions for option 2
-            #     self.interactive_new_lineup()
+            elif choice == "3":
+                # Perform actions for option 2
+                self.load_lineup()
             elif choice == "0":
                 print("Starting game.")
                 break  # Exit the loop
@@ -43,29 +44,39 @@ class Manager:
 
     def interactive_lineup_changes(self):
         # with print lineup and print bench set to true in bbgame it is not necessary to reprint them here
-       while True:
-           batting_order_number = int(input("\nEnter the batting order number to change (1-9), 0 accepts the lineup: "))
-           if batting_order_number == 0 or not isinstance(batting_order_number, (int, float, complex)):  # completed
-               break
+        while True:
+            print(self.team.print_starting_lineups())  # reprint line up and loop to unused pos players at top
+            print(self.team.print_pos_not_in_lineup())  # lineup already printed
+            batting_order_number = int(input("\nEnter the batting order number to change (1-9),"
+                                             " 0 accepts the lineup: "))
+            if batting_order_number == 0 or not isinstance(batting_order_number, (int, float, complex)):  # completed
+                break
 
-           player_index = int(input("Enter the index of the new player: "))
-           if isinstance(player_index, (int, float, complex)) and 1 <= batting_order_number <= 9:
-               self.team.change_lineup(target_pos=batting_order_number, pos_player_bench_index=player_index)
-               print("\nLineup updated!")
-           else:
-               print("Invalid input. Please enter valid batting order and player index numbers.")
-           print(self.team.print_starting_lineups())  # reprint line up and loop to unused pos players at top
-           print(self.team.print_pos_not_in_lineup())  # lineup already printed
-       return
+            player_index = int(input("Enter the index of the new player: "))
+            if isinstance(player_index, (int, float, complex)) and 1 <= batting_order_number <= 9:
+                self.team.change_lineup(target_pos=batting_order_number, pos_player_bench_index=player_index)
+                print("\nLineup updated!")
+            else:
+                print("Invalid input. Please enter valid batting order and player index numbers.")
+            # print(self.team.print_starting_lineups())  # reprint already printed
+        return
 
-    # def interactive_new_lineup(self):
-    #     lineup_dict = {}
-    #     sample_lineup = {65: 'LF', 71: 'C', 336: '1B', 369: 'DH', 355: 'CF', 62: 'SS', 536: '3B', 154: '2B', 310: 'RF'}
-    #     print(f'Enter player Number and Position for All Players one at a time example, e.g., {"65, LF"}: ')
-    #     for line_up_num in range(9):
-    #         player_index, fielding_pos = str(input(f'New Lineup {str(lineup_dict)} ')).split(',')
-    #         lineup_dict[player_index] = fielding_pos
-    #
-    #     self.team.set_initial_batting_order(lineup_dict)
-    #     self.team.set_prior_and_new_pos_player_batting_bench_dfs()  # ?? would prefer not to do this
-    #     return
+    def load_lineup(self):
+        # lineup_dict = {}
+        try:
+            with open('lineup.json', 'r') as f:
+                lineup_dict = json.load(f)
+        except FileNotFoundError:
+            lineup_dict = {65: 'LF', 71: 'C', 336: '1B', 369: 'DH', 355: 'CF', 62: 'SS',
+                           536: '3B', 154: '2B', 310: 'RF'}
+            with open('lineup.json', 'w') as f:
+                json.dump(lineup_dict, f)
+        print(lineup_dict)
+        self.team.set_initial_batting_order(lineup_dict)
+        self.team.set_prior_and_new_pos_player_batting_bench_dfs()  # ?? would prefer not to do this
+        return
+
+# print(f'Enter player Number and Position for All Players one at a time example, e.g., {"65, LF"}: ')
+#         for line_up_num in range(9):
+#             player_index, fielding_pos = str(input(f'New Lineup {str(lineup_dict)} ')).split(',')
+#             lineup_dict[player_index] = fielding_pos
